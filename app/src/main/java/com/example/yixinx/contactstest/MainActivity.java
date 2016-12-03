72904c7d6446450dcbbd764003659b3787eb92d9
EasonX <yixinx@andrew.cmu.edu>
2016-12-02 23:05:49 -0800

    [P1] Get info from phone's contact list

--- app/src/main/java/com/example/yixinx/contactstest/MainActivity.java
+++ app/src/main/java/com/example/yixinx/contactstest/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.yixinx.contactstest;
 
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView contactsView;
+    ArrayAdapter<String> adapter;
+    List<String> contactsList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactsView = (ListView)findViewById(R.id.contacts_view);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsList);
+        contactsView.setAdapter(adapter);
+        readContacts();
+    }
+
+    private void readContacts(){
+        Cursor cursor = null;
+        try{
+            cursor = getContentResolver().query(
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            while(cursor.moveToNext()){
+                //Get contact name
+                String contactName = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                ));
+                //Get contact phone number
+                String contactNumber = cursor.getString(cursor.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                ));
+                contactsList.add(contactName + "\n" + contactNumber);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if(cursor != null)
+                cursor.close();
+        }
     }
 }
