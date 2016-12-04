e76a96d8db8c784d300ce18ef7f750c6e8236cb8
EasonX <yixinx@andrew.cmu.edu>
2016-12-03 23:34:45 -0800

    [P2] Complete CRUD in MyProvider Class

--- app/src/main/java/com/example/yixinx/contactstest/MyProvider.java
+++ app/src/main/java/com/example/yixinx/contactstest/MyProvider.java
@@ -4,6 +4,7 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -15,7 +16,7 @@ public class MyProvider extends ContentProvider {
     public static final int BOOK_ITEM = 1;
     public static final int CATEGORY_DIR = 2;
     public static final int CATEGORY_ITEM = 3;
-    public static final String AUTHORITY = "com.example.yixinx.provider";
+    public static final String AUTHORITY = "com.example.contactstest.provider";
     private static UriMatcher uriMatcher;
     private MyDatabaseHelper dbHelper;
 
@@ -36,39 +37,45 @@ public class MyProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = null;
         switch (uriMatcher.match(uri)) {
-            case TABLE1_DIR:
-                // 查询table1表中的所有数据
+            case BOOK_DIR:
+                // 查询Book表中的所有数据
+                cursor = db.query("Book", projection, selection, selectionArgs, null, null, sortOrder);
                 break;
-            case TABLE1_ITEM:
-                // 查询table1表中的单条数据
+            case BOOK_ITEM:
+                // 查询book表中的单条数据
+                String bookId = uri.getPathSegments().get(1);
+                cursor = db.query("Book", projection, "id = ?", new String[]{ bookId }, null, null, sortOrder);
                 break;
-            case TABLE2_DIR:
-                // 查询table2表中的所有数据
+            case CATEGORY_DIR:
+                // 查询category表中的所有数据
+                cursor = db.query("Category", projection, selection, selectionArgs, null, null, sortOrder);
                 break;
-            case TABLE2_ITEM:
-                // 查询table2表中的单条数据
+            case CATEGORY_ITEM:
+                // 查询category表中的单条数据
+                String categoryId = uri.getPathSegments().get(1);
+                cursor = db.query("Category", projection, "id = ?", new String[] { categoryId }, null, null, sortOrder);
                 break;
             default:
                 break;
         }
-        return null;
+        return cursor;
     }
 
     @Nullable
     @Override
     public String getType(Uri uri) {
         switch (uriMatcher.match(uri)) {
-            case TABLE1_DIR:
-                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table1";
-            case TABLE1_ITEM:
-                return "vnd.android.cursor.item/vnd.com.example.app.provider.table1";
-            case TABLE2_DIR:
-                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table2";
-            case TABLE2_ITEM:
-                return "vnd.android.cursor.item/vnd.com.example.app.provider.table2";
-            default:
-                break;
+            case BOOK_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.contactstest.provider.book";
+            case BOOK_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.contactstest.provider.book";
+            case CATEGORY_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.contactstest.provider.category";
+            case CATEGORY_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.contactstest.provider.category";
         }
         return null;
     }
@@ -76,16 +83,67 @@ public class MyProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        SQLiteDatabase db = dbHelper.getWritableDatabase(); Uri uriReturn = null;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+            case BOOK_ITEM:
+                long newBookId = db.insert("Book", null, values);
+                uriReturn = Uri.parse("content://" + AUTHORITY + "/book/" + newBookId);
+                break;
+            case CATEGORY_DIR:
+            case CATEGORY_ITEM:
+                long newCategoryId = db.insert("Category", null, values);
+                uriReturn = Uri.parse("content://" + AUTHORITY + "/category/" + newCategoryId);
+                break;
+            default:
+                break; }
+        return uriReturn;
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = dbHelper.getWritableDatabase(); int deletedRows = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                deletedRows = db.delete("Book", selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Book", "id = ?", new String[] { bookId }); break;
+            case CATEGORY_DIR:
+                deletedRows = db.delete("Category", selection, selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                deletedRows = db.delete("Category", "id = ?", new String[]
+                        { categoryId });
+                break;
+            default:
+                break; }
+        return deletedRows;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        SQLiteDatabase db = dbHelper.getWritableDatabase(); int updatedRows = 0;
+        switch (uriMatcher.match(uri)) {
+            case BOOK_DIR:
+                updatedRows = db.update("Book", values, selection, selectionArgs);
+                break;
+            case BOOK_ITEM:
+                String bookId = uri.getPathSegments().get(1);
+                updatedRows = db.update("Book", values, "id = ?", new String[] { bookId });
+                break;
+            case CATEGORY_DIR:
+                updatedRows = db.update("Category", values, selection,
+                        selectionArgs);
+                break;
+            case CATEGORY_ITEM:
+                String categoryId = uri.getPathSegments().get(1);
+                updatedRows = db.update("Category", values, "id = ?", new String[] { categoryId });
+                break;
+            default:
+                break; }
+        return updatedRows;
     }
 }
