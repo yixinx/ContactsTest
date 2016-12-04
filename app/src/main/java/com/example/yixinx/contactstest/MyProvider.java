0c6991a05193fd0ccc768e4b175b9c84f592bd95
EasonX <yixinx@andrew.cmu.edu>
2016-12-03 23:27:15 -0800

    [P2] Data share between apps, add MyDatabaseHelper Class

---
+++ app/src/main/java/com/example/yixinx/contactstest/MyProvider.java
@@ -0,0 +1,91 @@
+package com.example.yixinx.contactstest;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by yixinx on 12/3/16.
+ */
+public class MyProvider extends ContentProvider {
+    public static final int BOOK_DIR = 0;
+    public static final int BOOK_ITEM = 1;
+    public static final int CATEGORY_DIR = 2;
+    public static final int CATEGORY_ITEM = 3;
+    public static final String AUTHORITY = "com.example.yixinx.provider";
+    private static UriMatcher uriMatcher;
+    private MyDatabaseHelper dbHelper;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "book", BOOK_DIR);
+        uriMatcher.addURI(AUTHORITY, "book/#", BOOK_ITEM);
+        uriMatcher.addURI(AUTHORITY, "category", CATEGORY_DIR);
+        uriMatcher.addURI(AUTHORITY, "category/#", CATEGORY_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new MyDatabaseHelper(getContext(), "BookStore.db", null, 2);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                // 查询table1表中的所有数据
+                break;
+            case TABLE1_ITEM:
+                // 查询table1表中的单条数据
+                break;
+            case TABLE2_DIR:
+                // 查询table2表中的所有数据
+                break;
+            case TABLE2_ITEM:
+                // 查询table2表中的单条数据
+                break;
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table1";
+            case TABLE1_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table1";
+            case TABLE2_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.app.provider.table2";
+            case TABLE2_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.app.provider.table2";
+            default:
+                break;
+        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
